cdb5988996f4944c4b2780a3ce1ab3d58c51b81d
crowetic <5431064+crowetic@users.noreply.github.com>
2022-03-03 14:40:19 -0800

    fix sinclairMATE image
    This script will make changes to sinclairMATE image to use new start script and modify crontab to restart Qortal Core every 5 days.

---
+++ sinclair-mate-fixes.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo BACKING UP CRONTAB
+
+crontab -l > cron.backup
+
+echo MAKING CHANGES TO QORTAL START SCRIPT
+
+cd /home/pi/qortal
+wget https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/start-modified-memory-args.sh
+mv start.sh original-start-script-sh
+mv start-modified-memory-args.sh start.sh
+chmod +x start.sh
+
+cd /home/pi
+
+echo DOWNLOADING AUTO-RESTART SCRIPT AND CONFIGURING CRON TO USE IT
+
+wget https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/restart-qortal.sh
+chmod +x restart-qortal.sh
+wget https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/restart-cron
+crontab restart-cron
+
+echo STOPPING QORTAL AND STARTING WITH NEW SCRIPT
+
+cd /home/pi/qortal
+./stop.sh && sleep 10 && killall -9 java
+./start.sh
