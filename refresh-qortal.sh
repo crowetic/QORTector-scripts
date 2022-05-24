79a1d4d1c0ca4c2d8d45797de185a92ac98c0ca9
crowetic <5431064+crowetic@users.noreply.github.com>
2022-05-24 09:57:11 -0700

    modify to use curl so it'll work on macs as well.

--- refresh-qortal.sh
+++ refresh-qortal.sh
@@ -6,5 +6,5 @@ sleep 5
 rm -R db
 rm qortal.jar
 rm log.t*
-wget https://github.com/qortal/qortal/releases/latest/download/qortal.jar
+curl -L -O https://github.com/qortal/qortal/releases/latest/download/qortal.jar
 ./start.sh
