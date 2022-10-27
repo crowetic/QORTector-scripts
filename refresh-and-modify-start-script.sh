2108dd53e822b58b058922c7105c756c8854ec54
crowetic <5431064+crowetic@users.noreply.github.com>
2022-10-27 16:07:24 -0700

    Create refresh-and-modify-start-script.sh
    New script to refresh-qortal and modify the start script for systems with lower RAM or memory crashes at the same time.

---
+++ refresh-and-modify-start-script.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd
+cd qortal
+./stop.sh
+sleep 5
+rm -R db
+rm start.sh
+rm qortal.jar
+rm log.t*
+curl -L -O https://github.com/qortal/qortal/releases/latest/download/qortal.jar
+curl -L -O https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/start-modified-memory-args.sh
+mv start-modified-memory-args.sh start.sh
+chmod +x start.sh
+./start.sh
