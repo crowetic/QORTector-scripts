570321c2f4ab6649e2b674321edf1b2063342554
crowetic <5431064+crowetic@users.noreply.github.com>
2022-12-05 12:51:17 -0800

    typo

--- setup-pi-automation.sh
+++ setup-pi-automation.sh
@@ -6,5 +6,5 @@ mate-terminal -- ./auto-fix-qortal.sh
 curl -L -O https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/auto-fix-cron
 crontab auto-fix-cron
 rm auto-fix-cron
-rm setup-pi-autmation.sh
+rm setup-pi-aut0mation.sh
 exit 1
