b951e89564c5d84c236458be8a54064d1025daff
crowetic <5431064+crowetic@users.noreply.github.com>
2022-12-05 12:51:31 -0800

    Update setup-pi-automation.sh

--- setup-pi-automation.sh
+++ setup-pi-automation.sh
@@ -6,5 +6,5 @@ mate-terminal -- ./auto-fix-qortal.sh
 curl -L -O https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/auto-fix-cron
 crontab auto-fix-cron
 rm auto-fix-cron
-rm setup-pi-aut0mation.sh
+rm setup-pi-automation.sh
 exit 1
