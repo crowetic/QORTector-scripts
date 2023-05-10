fee1b32c958995380fbc4e91f8f0687852abd018
crowetic <5431064+crowetic@users.noreply.github.com>
2023-05-10 12:20:38 -0700

    Update setup-automation-principal.sh

--- setup-automation-principal.sh
+++ setup-automation-principal.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 cd 
-curl -L -O https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/auto-fix-qortal.sh
+wget https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/auto-fix-qortal.sh
 chmod +x auto-fix-qortal.sh
+wget https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/start-qortal.sh 
+chmod +x start-qortal.sh
 gnome-terminal -- ./auto-fix-qortal.sh
-curl -L -O https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/auto-fix-cron-principal
+wget https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/auto-fix-cron-principal
 crontab auto-fix-cron-principal
 rm auto-fix-cron-principal
 rm setup-automation-principal.sh
