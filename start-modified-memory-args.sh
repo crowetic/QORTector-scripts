2f3c220cf6d0d9b85e4a25c8a9a92cfc0e48a61e
crowetic <5431064+crowetic@users.noreply.github.com>
2023-08-20 16:44:34 -0700

    Update start-modified-memory-args.sh
    few modifications for memory and niceness

--- start-modified-memory-args.sh
+++ start-modified-memory-args.sh
@@ -33,13 +33,13 @@ fi
 # Limits Java JVM stack size and maximum heap usage.
 # Comment out for bigger systems, e.g. non-routers
 # or when API documentation is enabled
-JVM_MEMORY_ARGS="-Xss2048k -Xmx2350m"
+JVM_MEMORY_ARGS="-Xss1256k -Xmx1750m"
 
 # Although java.net.preferIPv4Stack is supposed to be false
 # by default in Java 11, on some platforms (e.g. FreeBSD 12),
 # it is overridden to be true by default. Hence we explicitly
 # set it to false to obtain desired behaviour.
-nohup nice -n 20 java \
+nohup nice -n 5 java \
 	-Djava.net.preferIPv4Stack=false \
 	${JVM_MEMORY_ARGS} \
 	-jar qortal.jar \
