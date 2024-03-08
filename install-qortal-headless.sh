3e600a6ca800ba032590afe7b6feebc0e1ee906a
crowetic <5431064+crowetic@users.noreply.github.com>
2024-03-08 12:29:20 -0800

    Create install-qortal-headless.sh
    new installation script for debian-based headless machines. 
    
    NOTE - This is just a simple script quickly written. Updates and/or new scripts will be done in the future that will automatically check for OS, then install based on that.

---
+++ install-qortal-headless.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# Regular Colors
+BLACK='\033[0;30m'        # Black
+RED='\033[0;31m'          # Red
+GREEN='\033[0;32m'        # Green
+YELLOW='\033[0;33m'       # Yellow
+BLUE='\033[0;34m'         # Blue
+PURPLE='\033[0;35m'       # Purple
+CYAN='\033[0;36m'         # Cyan
+WHITE='\033[0;37m'        # White
+NC='\033[0m'              # No Color
+
+echo "${YELLOW} UPDATING AND INSTALLING REQUIRED SOFTWARE PACKAGES ${NC}\n" 
+
+sudo apt update 
+sudo apt -y upgrade
+sudo apt -y install unzip vim curl openjdk-17-jre p7zip-full htop net-tools bpytop ffmpeg sysbench smartmontools
+# TODO - check if the system is ubuntu, if so then remove the bullshit ubuntu-advantage-tools, then re-install gnome-software.
+#sudo apt -y --purge remove ubuntu-advantage-tools 
+#sudo apt -y install gnome-software
+
+echo "${YELLOW} DOWNLOADING QORTAL CORE AND QORT SCRIPT ${NC}\n"
+
+curl -L -O https://github.com/Qortal/qortal/releases/latest/download/qortal.zip
+unzip qortal*.zip
+rm qortal*.zip
+cd qortal
+rm settings.json
+curl -L -O https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/settings.json
+chmod +x *.sh
+curl -L -O https://raw.githubusercontent.com/Qortal/qortal/master/tools/qort
+chmod +x qort
+
+echo "${YELLOW} DOWNLOADING OTHER CUSTOM SCRIPTS FOR QORTAL ${NC}\n"
+cd 
+curl -L -O https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/refresh-qortal.sh
+curl -L -O https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/auto-fix-qortal.sh
+curl -L -O https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/check-qortal-status.sh
+curl -L -O https://raw.githubusercontent.com/crowetic/QORTector-scripts/main/start-qortal.sh
+
+
+chmod +x *.sh
+mv check-qortal-status.sh ~/qortal
+
+echo "${YELLOW} FINISHING UP ${NC}\n"
+
+echo "${YELLOW} REBOOTING MACHINE IN 10 SECONDS${NC} - ${RED}IF YOU WOULD NOT LIKE TO REBOOT, PUSH${NC} ${GREEN} CNTRL+C${NC}${RED} WITHIN THE NEXT 10 SECONDS${NC}\n - ${YELLOW}IF YOU WOULD LIKE TO${NC} ${CYAN}START QORTAL AT BOOT${NC}${YELLOW} ADD THE FOLLOWING TO CRON${NC}'${GREEN} @reboot ./start-qortal.sh${NC}' -${YELLOW} YOU CAN ACCESS CRON EDITOR WITH${NC} '${GREEN}crontab -e${NC}\n'" 
+
+sleep 10 
+
+sudo reboot
