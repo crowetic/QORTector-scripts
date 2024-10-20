3e81b1cd8213492acaf0e743e126d06f8adcb197
crowetic <jason@crowetic.com>
2024-10-20 10:02:17 -0700

    added every hour restart script

---
+++ restart-core-every-hour.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Path to the Qortal folder
+QORTAL_DIR=~/qortal
+
+while true; do
+  # Navigate to Qortal directory
+  cd "$QORTAL_DIR" || exit
+
+  # Stop Qortal core
+  ./stop.sh
+
+  # Wait for 45 seconds
+  sleep 45
+
+  # Kill all Java processes
+  killall -9 java
+
+  # Remove blockchain lock file
+  rm -rf "$QORTAL_DIR/db/blockchain.lck"
+
+  # Start Qortal core
+  ./start.sh
+
+  # Wait for 2 hours before restarting again, while tailing the log file
+  sleep 1h &
+  tail -f "$QORTAL_DIR/qortal.log"
+
+  # Kill the tail process after sleep is done
+  kill $!
+done
+
