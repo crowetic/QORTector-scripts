27fd1e77ae19545a0b4d678027309b256b40d0df
crowetic <5431064+crowetic@users.noreply.github.com>
2024-11-10 10:30:10 -0800

    Update start-6001-to-16000m.sh

--- start-6001-to-16000m.sh
+++ start-6001-to-16000m.sh
@@ -33,7 +33,7 @@ fi
 # Limits Java JVM stack size and maximum heap usage.
 # Comment out for bigger systems, e.g. non-routers
 # or when API documentation is enabled
-JVM_MEMORY_ARGS="-Xss1024m -Xmx4128m -XX:MaxRAM=4172m -XX:+UseSerialGC"
+JVM_MEMORY_ARGS="-XX:MaxRAMPercentage=40 -XX:+UseG1GC -Xss512k -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./heapdump.hprof -Xlog:gc*:file=gc.log:time,uptime,level,tags"
 
 # Although java.net.preferIPv4Stack is supposed to be false
 # by default in Java 11, on some platforms (e.g. FreeBSD 12),
