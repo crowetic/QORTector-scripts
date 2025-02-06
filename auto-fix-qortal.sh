0b8fd6b332b74b7dafc2f62443b4375202585df4
crowetic <jason@crowetic.com>
2025-02-05 17:56:35 -0800

    updated auto-fix script again

--- auto-fix-qortal.sh
+++ auto-fix-qortal.sh
@@ -429,7 +429,7 @@ update_script() {
     sleep 2
     potentially_update_settings
     rm -rf ${HOME}/qortal.jar ${HOME}/run.pid ${HOME}/run.log ${HOME}/remote.md5 ${HOME}/qortal/local.md5
-    mkdir -P ${HOME}/backups && mv ${HOME}/qortal/backup-settings* ${HOME}/backups
+    mkdir -p ${HOME}/backups && mv ${HOME}/qortal/backup-settings* ${HOME}/backups
     echo "${YELLOW}Auto-fix script run complete.${NC}\n"
     sleep 5
     exit
